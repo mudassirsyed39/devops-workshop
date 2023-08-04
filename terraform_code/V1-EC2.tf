637b8a504551919398d16258a24c72fc9c69c45e
Mudassir Syed <45922164+mudassirsyed39@users.noreply.github.com>
2023-08-04 17:42:31 +0530

    Update V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXL5BGI2355ZIWBO7"
-  secret_key = "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"
 }
 resource "aws_instance" "demo-server" {
     ami = "ami-0f34c5ae932e6f0e4"
