d2ae630e62fb4f09ef81ca859c83f4998d3f2549
mudassirsyed <mudassirsyed39@gmail.com>
2023-08-04 17:02:10 +0530

    commiting V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "AKIAXL5BGI2355ZIWBO7"
+  secret_key = "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"
 }
-
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-0f34c5ae932e6f0e4"
     instance_type = "t2.micro"
     key_name = "dpp"
 }
